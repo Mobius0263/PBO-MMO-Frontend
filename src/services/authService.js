24e151672de966dc29f27d26f7966d287d091caf
Mobius0263 <mahendra33115@gmail.com>
2025-07-26 20:08:13 +0700

    Update auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = "https://meet-up.railway.internal/login";
 
 // Create axios instance
 const api = axios.create({
